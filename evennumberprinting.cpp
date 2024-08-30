e01f98ca70ac1e8aef0df53dae00d6ff872cbdce
gargipedhekar <gargi27.pedhekar@gmail.com>
2024-08-30 12:04:59 +0530

    Create evennumberprinting.cpp

---
+++ evennumberprinting.cpp
@@ -0,0 +1,24 @@
+//Name:Gargi Pedhekar
+//PRN:23070123052
+//AIM - To print even number using loops.
+#include <iostream>
+using namespace std;
+
+int main() {
+ for(int i=0;i<11;i++)
+{if(i%2==0)
+{cout << i<< endl;}}
+
+    return 0;
+}
+
+/*Output-
+0
+2
+4
+6
+8
+10
+
+
+*/
