068d33e84918cc7e244f5345f945485371d3e310
gargipedhekar <gargi27.pedhekar@gmail.com>
2024-08-30 12:12:42 +0530

    Create pyramidalpattern.cpp

---
+++ pyramidalpattern.cpp
@@ -0,0 +1,19 @@
+//Name:Gargi
+//PRN:23070123052
+//Aim - Reversing PRN using loops.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int PRN,num;
+
+    cout <<"Enter PRN: "<<endl;
+    cin>>PRN;
+    while(PRN>0)
+    {
+        num = PRN%10;
+        PRN = PRN/10;
+        cout <<num;
+    }
+    return 0;
+}
